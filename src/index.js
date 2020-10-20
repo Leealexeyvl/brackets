ee04cf36eb94c6735758b884fc4caa77fef75606
leealexeyvl <leealexey_94@mail.ru>
2020-10-20 11:36:58 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = str.slice();
+  let bracketsArray = [];
+  let param;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    bracketsArray.push(bracketsConfig[i].join(''));
+  }
+
+  while (bracketsArray.some(function(elem) {
+    param = result.indexOf(elem);
+    return result.indexOf(elem) != -1})) {
+    result = result.replace(result.substring(param, param + 2), '');
+  }
+
+ return result.length == 0;
 }
